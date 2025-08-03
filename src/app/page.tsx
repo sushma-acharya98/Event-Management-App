c404a339a805a51582d3718191d95617106ded13
Sushma Acharya <sushmaacharya345@gmail.com>
2025-08-03 15:30:59 +0545

    Add css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,17 @@ import EventList from "@/components/EventList";
 
 const Home = () => {
     return (
-        <div className="event_box" style={{ padding: "2rem" }}>
-            <h1>Event Manager</h1>
-            <Link href="/add">
-                <Button variant="contained" sx={{ mb: 2 }}>
-                    Add Event
-                </Button>
-            </Link>
+        <div className="event-box">
+            <div className="event-box-header">
+                <h1>Event Management App</h1>
+            </div>
+            <div className="event-box-button">
+                <Link href="/add">
+                    <Button variant="contained" sx={{ mb: 2 }}>
+                        Add Event
+                    </Button>
+                </Link>
+            </div>
             <EventList />
         </div>
     );
